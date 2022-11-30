aa49409e4f26439791d791f6632d9c52ee603d4e
dediane <ddecourt@student.42.fr>
2022-11-30 21:51:18 +0100

    add commands files

---
+++ server/command/away.cpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   away.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 15:13:32 by ddecourt          #+#    #+#             */
+/*   Updated: 2022/11/23 15:14:45 by ddecourt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*          /away [message]
+Leaves a message indicating that you are currently not paying attention to IRC. 
+When someone sends you a message, they will automatically see your away message. 
+Using /AWAY with no message marks you as no longer being away and removes your previous message.
+
+example:  /away Off to get something to eat, back in a moment.*/
---
+++ server/command/join.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   join.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 15:15:15 by ddecourt          #+#    #+#             */
+/*   Updated: 2022/11/23 15:16:05 by ddecourt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// COMMANDE: /join #channel
+// Joins the specified channel.
+// example:  /join #irchelp
+
+// This will make you join the #irchelp channel. Once on a channel, anything you type will be seen by all the users on this channel. 
+// The #irchelp channel is very useful, so say hello and then ask any questions you want. 
+// If the channel you specified does not exist, a channel with that name will be created for you.
+// Some channels may also have keys ie. a password, which you need to specify when using the /join command.
+// example:  /join #irchelp trout
+
+// This will make you join the #irchelp channel using "trout" as the password.
