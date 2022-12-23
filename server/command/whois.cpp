eb58712f01f74deac92297f39c9f88ea915f8d6b
Marie-Ines Loubet <mloubet@student.42.fr>
2022-12-23 23:03:24 +0100

    ok whois, reste a return le bufer des messages d'erreurs pour nettoyer

--- server/command/whois.cpp
+++ server/command/whois.cpp
@@ -20,68 +20,15 @@ void Command::whois(Message *msg, std::vector<std::string> message)
     std::cout << "Je suis dans WHOIS" << std::endl;
     User *user = msg->getuser();
     Server *server = msg->getserver();
-    std::cout << "MESSAGE SIZE IS : " << message.size() << std::endl;
-    std::vector<std::string>::iterator it;
-    for (it  = message.begin(); it != message.end(); it++ )
-            std::cout << " ==== " << *it << std::endl;
-(void)server;
-   
-        // std::cout << "REAL NAME IS " <<  user->getUsername() << std::endl;
-        // std::vector<std::string>::iterator it = std::find(users.begin(), users.end(),  message[2]);
-        // if (it != users.end())
-        //     send_reply(user->getFd(),   user->getNickname() + " " + user->getUsername() + " " + user->getHostname() + " " + "*" + " :" + user->getRealname()  + END); //311,
-
-
-    std::cout << "REAL NAME IS " <<  user->getUsername() << std::endl;
     std::vector<std::string> users = server->get_all_nicknames();
-    std::cout << "SEARCHING FOR " << message[1] << std::endl;
-        std::vector<std::string>::iterator it2 = std::find(users.begin(), users.end(),  message[1]);
-        if (it2 != users.end())
-        {
-            std::cout << "J AI TROUVE " << std::endl;
-            User * user2 = server->get_user_by_nickname(message[1]);
-            // send_reply(user->getFd(),   user->getNickname() + " " + user->getUsername() + " " + user->getHostname() + " " + "*" + " :" + user->getRealname()  + END); //311,
-            if (user2)
-                send_reply(user->getFd(),   user2->getNickname() + " " + user2->getUsername() + " " + user2->getHostname() + " " + "*" + " :" +  user2->getRealname()  + END); //311,
-
-        } // "<nick> <user> <host> * :<real name>"
-
-            // send_reply(user->getFd(),   (*it).getNickname() + " " + (*it).getUsername() + " " + (*it).getHostname() + " " + "*" + " :" + (*it).getRealname()  + END); //311,
-//     RPL_WHOISUSER::RPL_WHOISUSER(Peer &peer, const Peer &target, bool include_prefix) : Message(peer, std::string())
-
-    // else if ((message.size() == 2))
-    // {
-    //     // std::vector<std::string> users =  server->getUsers();
-    //     std::vector<std::string> users = server->get_all_nicknames();
-    //     std::vector<std::string>::iterator it = std::find(users.begin(), users.end(),  message[2]);
-    //     if (it != users.end())
-    //     {
-    //         std::cout << "REAL NAME IS " << *it << std::endl;
-    //         send_reply(user->getFd(), user->getPrefix() + " WHOIS " +  (*it) + END); 
-            
-    //         //PRINT  (*it).getUsername()
-    //          //RPL_WHO"<nick> <user> <host> * :<real name>"
-    //         // 311    RPL_WHOISUSER
-    //     }
-
-    //     // std::map<int, User> users =  server->getUsers();
-    //     // std::map<int, User>::iterator it = users.find(message[2]);
-    //     // if (it != users.end())
-    //     // {
-    //     //     send_reply(user->getFd(), user->getPrefix() + " WHOIS " +  (*it).getUsername() + END); 
-    //     //     //PRINT  (*it).getUsername()
-    //     //      //RPL_WHO"<nick> <user> <host> * :<real name>"
-    //     //     // 311    RPL_WHOISUSER
-    //     // }
-
-    // }
-
-   
-    // "<nick> <user> <host> * :<real name>"
-    // 312    RPL_WHOISSERVER
-    // "<nick> <server> :<server info>"
-    // 313    RPL_WHOISOPERATOR
-    // "<nick> :is an IRC operator"
-
-
+    std::vector<std::string>::iterator it = std::find(users.begin(), users.end(),  message[1]);
+    if (it != users.end())
+    {
+        User * user2 = server->get_user_by_nickname(message[1]);
+        if (user2)
+            send_reply(user->getFd(),   user2->getNickname() + " " + user2->getUsername() + " " + user2->getHostname() + " " + "*" + " :" +  user2->getRealname()  + END); //311  RPL_WHOISUSER // "<nick> <user> <host> * :<real name>"
+        send_reply(user->getFd(), user->getNickname() + " " + server->getCreationTime() + END);// 312    RPL_WHOISSERVER // "<nick> <server> :<server info>" //autres infos du serveur a mettre en avant ?
+        if (user2->getMode() == "+o") //a verifier apres qu'on s'occupe des operators
+            send_reply(user->getFd(), user2->getNickname() + " :is an IRC operator" + END);  // 313    RPL_WHOISOPERATOR // "<nick> :is an IRC operator"
+    }
 }
