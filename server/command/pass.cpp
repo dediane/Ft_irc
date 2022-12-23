aada3b050d5dbd673058bffd56e6e46d78222bf4
balkisous <balkisous@gmail.com>
2022-12-23 16:36:25 +0100

    gestion de pass

--- server/command/pass.cpp
+++ server/command/pass.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pass.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bben-yaa <bben-yaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 15:58:09 by ddecourt          #+#    #+#             */
-/*   Updated: 2022/12/15 17:08:45 by ddecourt         ###   ########.fr       */
+/*   Updated: 2022/12/23 16:28:06 by bben-yaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ void Command::pass(Message *msg, std::vector<std::string> message)
     if (message.size() == 1)
         return(send_reply(user->getFd(), ERR_NEEDMOREPARAMS(message[0])));
     if (message[1] != serverPassword)
+    {
+        user->setisOnline(false);
         return(send_reply(user->getFd(), ERR_PASSWDMISMATCH()));
+    }
     if (message[1] == serverPassword)
         user->setisPassword(true);
     std::cout << "Je passe dans la commande PASS" << std::endl;
