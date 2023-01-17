9dfcefd26010686db4e983e12e6d2c19f2384c3e
balkisous <balkisous@gmail.com>
2023-01-17 13:44:11 +0100

    bon message d'error

--- server/command/privmsg.cpp
+++ server/command/privmsg.cpp
@@ -6,7 +6,7 @@
 /*   By: bben-yaa <bben-yaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 13:27:00 by ddecourt          #+#    #+#             */
-/*   Updated: 2023/01/17 11:15:10 by bben-yaa         ###   ########.fr       */
+/*   Updated: 2023/01/17 13:42:13 by bben-yaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,12 +54,12 @@ void Command::privmsg(Message *msg, std::vector<std::string> message)
     if (channel->is_mode('n'))
     {
         if (!channel->isUserinChannel(*user))
-            return (send_reply(user->getFd(), user->getPrefix() + " 442 " + ERR_NOTONCHANNEL(channel->getName())));
+            return (send_reply(user->getFd(), user->getPrefix() + " 404 " + ERR_CANNOTSENDTOCHAN(channel->getName())));
     }
     if (channel->is_mode('m'))
     {
         if (!channel->is_usermode(user->getFd(), 'v') || !channel->is_usermode(user->getFd(), 'o'))
-   		    return (send_reply(user->getFd(), user->getPrefix() + " 442 " + user->getNickname() + " " + ERR_NOTONCHANNEL(channel->getName())));
+   		    return (send_reply(user->getFd(), user->getPrefix() + " 404 " + user->getNickname() + " " + ERR_CANNOTSENDTOCHAN(channel->getName())));
     }
     buffer.erase(0, 2);
     channel->broadcast_msg(user->getPrefix() + " PRIVMSG " + channel_name + " " + buffer + END, user);
